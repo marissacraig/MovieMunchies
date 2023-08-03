593e5c931c539e31aa3b5536e547b65f5fd4fd92
marissacraig <135668035+marissacraig@users.noreply.github.com>
2023-08-02 18:45:06 -0700

    fixed display error on save.js

--- assets/save.js
+++ assets/save.js
@@ -36,52 +36,26 @@ function reload() {
         recipeNameEl.textContent = recipeName;
         // Split and join recipeIngredients to display on separate lines
         document.getElementById('ingredientDescription').innerHTML = combo.recipeIngredients.replace(/\,/g, '<br>');
+
+        
         }
     }
-
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
-    });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-  
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-      if (event.code === 'Escape') {
-        closeAllModals();
-      }
-    });
-
-    // Create a huge button
-    const listItem = document.createElement('li');
-        listItem.classList.add('mb-3');
+       // Create a huge button
+       const listItem = document.createElement('li');
+       listItem.classList.add('mb-3');
    
-    const columnId = `column0`;
-    const column = document.getElementById(columnId);
-    column.appendChild(listItem);
-    const hugeButton = document.createElement('button');
-    hugeButton.classList.add('button', 'is-primary', 'is-large', 'js-modal-trigger');
-    hugeButton.dataset.target.add('modal-trigger');
-    hugeButton.textContent = movieName +' / '+ recipeName;
-    hugeButton.setAttribute('id', combos[i]);
-        const id = this.getAttribute('id');
-        const data = localStorage.getItem(id);
-    
-    listItem.appendChild(hugeButton);
-
+       const columnId = `column0`;
+       const column = document.getElementById(columnId);
+       column.appendChild(listItem);
+       const hugeButton = document.createElement('button');
+       hugeButton.classList.add('button', 'is-primary', 'is-large', 'js-modal-trigger');
+       hugeButton.dataset.target = 'modal-trigger';
+       hugeButton.textContent = movieName +' / '+ recipeName;
+       
+      
+       
+listItem.appendChild(hugeButton);
+       
     // Create a small delete button
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('button', 'is-danger', 'is-small');
@@ -110,8 +84,35 @@ function reload() {
         var combosString = JSON.stringify(combos);
         localStorage.setItem('combos', combosString);
     });
-    listItem.appendChild(deleteButton);
 
+  
+    
+    listItem.appendChild(deleteButton);
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+    
+        $trigger.addEventListener('click', () => {
+          openModal($target);
+        });
+      });
+    
+      // Add a click event on various child elements to close the parent modal
+      (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+    
+        $close.addEventListener('click', () => {
+          closeModal($target);
+        });
+      });
+    
+      // Add a keyboard event to close all modals
+      document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+          closeAllModals();
+        }
+      });
    
    
 
